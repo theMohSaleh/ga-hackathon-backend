012ddfb0dd5a9b1c5ec652991579407aa7df1e5c
halbuflasa <albuflasahessa@gmail.com>
2024-11-24 14:50:31 +0300

    product controller

---
+++ controllers/products.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const Product = require('../models/Product');
+const verifyToken = require('../middleware/verify-token.js');
+const router = express.Router();
+
+// ========== Public Routes ===========
+
+//GET /products - Get a list of all products
+router.get('/', async (req, res) => {
+    try {
+      const productsFound = await Product.find();
+      if (!productsFound.length) {
+        res.status(404);
+        throw new Error('No products found');
+      }
+      res.status(200).json(productsFound);
+    } catch (error) {
+      if (res.statusCode === 404) {
+        res.json({ error: error.message });
+      } else {
+        res.status(500).json({ error: error.message });
+      }
+    }
+  });
+
+ // GET /products/:productId - Get details of a specific product
+ router.get('/:productId', async (req, res) => {
+    try {
+      const foundProduct = await Product.findById(req.params.productId);
+      if (!foundProduct) {
+        res.status(404);
+        throw new Error('Product not found.');
+      }
+      res.status(200).json(foundProduct);
+    } catch (error) {
+      if (res.statusCode === 404) {
+        res.json({ error: error.message });
+      } else {
+        res.status(500).json({ error: error.message });
+      }
+    }
+  });
+  
+module.exports = router
